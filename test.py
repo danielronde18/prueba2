941560b3bd9793b1c44155b158c01797932aaca1
danielronde18 <cd.ronderos@uniandes.edu.co>
2018-11-21 18:31:19 +0000

    modificado

--- test.py
+++ test.py
@@ -5,3 +5,18 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 print("hola mundo ")
+
+x=np.zeros((2,2),float)
+print(x)
+
+x1=np.linspace(-1,1,100)
+def f(x):
+    return x**2
+
+
+
+
+plt.figure()
+plt.plot(x1,f(x1))
+plt.show
+plt.savefig("curva")
